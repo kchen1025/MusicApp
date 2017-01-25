9f3410e7e05d390d353303bf175ea9ad34ac3128
Kevin Chen <kchen1025@gmail.com>
2017-01-24 17:18:21 -0800

    added password validations and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get '/about', to:'static_pages#about'
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
